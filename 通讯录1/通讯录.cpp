9ad27a1c8806500fb81ff0d386d9dc1c622ee372
宇辰 陆 <3179553656@qq.com>
2024-11-24 21:00:24 +0800

    更新

--- 通讯录1/通讯录.cpp
+++ 通讯录1/通讯录.cpp
@@ -61,7 +61,90 @@ void addpeople(phonebook* p)
 		system("cls");//	清屏
 	}
 }
+int showpeople(phonebook* p) {
+	if (p->total == 0) {
+		cout << "当前记录为空" << endl;
+		return -1;
+	}
+	else {
+		for (int i = 0; i < p->total; i++) {
+			cout << "姓名:" << p->peo[i].name<<"\t";
+			cout << "年龄:" << p->peo[i].age << "\t";
+			cout << "电话:" << p->peo[i].number << "\t";
+			cout << "性别:" << (p->peo[i].sex==1?"男":"女") << "\t";
+			cout << "住址:" << p->peo[i].home << endl;
+		}
+	}
 
+}
+int isExist(phonebook* p, string name) {//检查这个人是否存在，若存在返回1，不存在返回0
+	for (int i = 0; i <= p->total; i++) {
+		if (name == p->peo[i].name)return i;
+	}
+	return -1;
+}
+void deltPeople(phonebook* p, string name) {
+	
+	if (isExist(p, name) == -1) {
+		cout << "没这个人" << endl;
+	}
+	else {
+		cout << "已删除" << endl;
+		int state = isExist(p, name);
+		for (int i = state; i < p->total; i++) {
+			p->peo[i] = p->peo[i + 1];
+		}
+		p->total--;
+	}
+	system("pause");
+	system("cls");
+}
+
+void findPeople(phonebook* p, string name) {
+	int state;
+	state = isExist(p, name);
+	if (state == -1) {
+		cout << "查无此人" << endl;
+	}
+	else {
+		cout << "姓名:" << p->peo[state].name << "\t";
+		cout << "年龄:" << p->peo[state].age << "\t";
+		cout << "电话:" << p->peo[state].number << "\t";
+		cout << "性别:" << (p->peo[state].sex == 1 ? "男" : "女") << "\t";
+		cout << "住址:" << p->peo[state].home << endl;
+	}
+}
+
+void chanemessage(phonebook* p, string name) {
+	int state = isExist(p, name);
+	if (state == -1) {
+		cout << "不存在此人" << endl;
+	}
+	else {
+		int sex = 0;
+		cout << "请输入姓名：";
+		cin >> p->peo[state].name;
+		cout << "请输入年龄：";
+		cin >> p->peo[state].age;
+		while (sex != 1 && sex != 2) {
+			cout << "请输入性别：（1代表男，2代表女）";
+			cin >> sex;
+			p->peo[state].sex = sex;
+		}
+		cout << "请输入住址：";
+		cin >> p->peo[state].home;
+		cout << "请输入电话号：";
+		cin >> p->peo[state].number;
+		cout << "修改成功" << endl;
+	}
+}
+
+void clearPeople(phonebook* p) {
+	p->total = 0;
+	cout << "联系人已经清空" << endl;
+	system("pause");
+	system("cls");
+}
 int main() {
 	int select_state = 1;
 	int select = 0;//创建用户选择输入的变量
@@ -76,14 +159,53 @@ int main() {
 			addpeople(&book);
 			break;
 		case 2://显示联系人
+		{	
+			showpeople(&book);
+			system("pause");
+			system("cls"); 
+		}
 			break;
 		case 3://删除联系人
+		{
+			int state;
+			state=showpeople(&book);
+			string name;
+			if(state!=-1)
+			{
+				cout << "请输入你想删除的人的姓名:";
+				cin >> name;
+				deltPeople(&book, name);
+			}
+		}
 			break;
 		case 4://查找联系人
+		{
+			string name;
+			cout << "请输入你想查找的人的姓名:";
+			cin >> name;
+			findPeople(&book, name);
+			system("pause");
+			system("cls");
+		}
+			
 			break;
 		case 5://修改联系人
+		{
+			showpeople(&book);
+			string name;
+			if(book.total!=0)
+			{
+				cout << "请输入你想修改的人的姓名:";
+				cin >> name;
+				chanemessage(&book, name);
+			}
+
+			system("pause");
+			system("cls");
+		}
 			break;
 		case 6://清空联系人
+			clearPeople(&book);
 			break;
 		case 0:
 			cout << "欢迎下次使用" << endl;
